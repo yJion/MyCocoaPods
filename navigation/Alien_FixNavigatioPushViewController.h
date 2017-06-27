a0da75fde175f0b3414e3b2d51aba982340522fc
alien <alien@aliendeiMac.local>
2017-06-27 14:55:26 +0800

    no message

---
+++ navigation/Alien_FixNavigatioPushViewController.h
@@ -0,0 +1,13 @@
+//
+//  Alien_FixNavigatioPushViewController.h
+//  Purse
+//
+//  Created by alien on 16/9/26.
+//  Copyright © 2016年 alien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Alien_FixNavigatioPushViewController : UINavigationController
+
+@end
